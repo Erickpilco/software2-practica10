ee67793e90bc746a2c3e600df82be5bfa7b698a6
mathiassejje21 <mathiassejje@gmail.com>
2025-07-06 22:25:23 -0500

    Agregar en feature/login

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import productoRoutes from "./routes/productoRoutes.js";
 import categoriaRoutes from "./routes/categoriaRoutes.js";
 import * as path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = new express();
 const __filename = fileURLToPath(import.meta.url);
@@ -38,6 +39,7 @@ conectarDB().then(() => {
   app.use("/api/pacientes/", pacienteRoutes);
   app.use("/api/productos/", productoRoutes);
   app.use("/api/categorias/", categoriaRoutes);
+  app.use("/api/auth", authRoutes);
 });
 
 app.listen(PORT, () => {
