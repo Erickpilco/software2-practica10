894b0fc53cdaf559d81694764cd0021f49bfd835
mathiassejje21 <mathiassejje@gmail.com>
2025-07-06 22:06:58 -0500

    nuevo cambio - primera rama

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,47 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
 import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 import pacienteRoutes from "./routes/pacienteRoutes.js";
-import cors from "cors"
+import cors from "cors";
 import productoRoutes from "./routes/productoRoutes.js";
 import categoriaRoutes from "./routes/categoriaRoutes.js";
 import * as path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
 
-const app = new express()
+const app = new express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config()
+dotenv.config();
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
-const dominiosPermitidos = [process.env.FRONTEND_URL]
+const dominiosPermitidos = [process.env.FRONTEND_URL];
 
 const corsOptions = {
-    origin: function (origin, callback){
-        if (true){
-                callback(null,true)
-        }else{
-            callback(new Error("No permitido por CORS"))
-        }
+  origin: function (origin, callback) {
+    if (true) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
     }
-}
+  },
+};
 
-app.use(cors(corsOptions))
-conectarDB().then(()=>{
-    app.use(express.json())
-    app.use("/api/veterinarios/", veterinarioRoutes)
-    app.use("/api/pacientes/", pacienteRoutes)
-    app.use("/api/productos/", productoRoutes)
-    app.use("/api/categorias/", categoriaRoutes)
-})
+app.use(cors(corsOptions));
+conectarDB().then(() => {
+  app.use(express.json());
+  app.use("/api/veterinarios/", veterinarioRoutes);
+  app.use("/api/pacientes/", pacienteRoutes);
+  app.use("/api/productos/", productoRoutes);
+  app.use("/api/categorias/", categoriaRoutes);
+  app.use("/api/auth", authRoutes);
+});
 
-
-app.listen(PORT,()=>{
-    console.log(`Servidor iniciado correctamente en el puerto: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado correctamente en el puerto: ${PORT}`);
+});
